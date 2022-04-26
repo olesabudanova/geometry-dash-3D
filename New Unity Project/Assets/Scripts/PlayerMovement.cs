82e86643f167c54e1d4c35060cbcd132611fd0bd
olesabudanova <99878197+olesabudanova@users.noreply.github.com>
2022-04-26 20:08:29 +0300

    Added deformation

--- New Unity Project/Assets/Scripts/PlayerMovement.cs
+++ New Unity Project/Assets/Scripts/PlayerMovement.cs
@@ -1,5 +1,16 @@
 using UnityEngine;
 
+public struct Limiter
+{
+    public Limiter(Vector3 min, Vector3 max)
+    {
+        MIN = min; 
+        MAX = max;
+    }
+    public Vector3 MIN { get; }
+    public Vector3 MAX { get; }
+}
+
 public class PlayerMovement : MonoBehaviour
 {
     // This is a reference to the Rigidbody component called "rb"
@@ -8,12 +19,19 @@ public class PlayerMovement : MonoBehaviour
     public float forwardForce = 2000f;
     public float sidewaysForce = 500f;
 
+    private bool isTransforming = false;
+    public Limiter limiter = new Limiter(new Vector3(1f, 0.5f, 1f), new Vector3(15f, 1f, 1f));
+    public float xScaleTrigger = 3f;
+    private Vector3 xScaleChange = new Vector3(0.05f, 0f, 0f);
+    private Vector3 yScaleChange = new Vector3(0f, 0.05f, 0f);
+
     // We marked this as "Fixed"Update becase we 
     // are using it to mess with physics.
     void FixedUpdate()
     {
         // Add a forward force 
-        rb.AddForce(0, 0, forwardForce * Time.deltaTime);  
+        rb.AddForce(0, 0, forwardForce * Time.deltaTime);
+        CheckValues();
         
         if (Input.GetKey("d")) 
         {
@@ -32,7 +50,47 @@ public class PlayerMovement : MonoBehaviour
 
         if (Input.GetKey("u"))
         {
-            transform.scale.
+            Deform();
+        }
+
+        if (Input.GetKey("i"))
+        {
+            Deform(true);
+        }
+    }
+
+    private void Deform(bool reverse = false)
+    {
+        
+        transform.localScale += reverse ? -xScaleChange : xScaleChange;
+
+       // if (transform.localScale.x > xScaleTrigger)
+        
+            transform.localScale += reverse ? yScaleChange : -yScaleChange;
+        
+
+        
+
+    }
+
+    private void CheckValues()
+    {
+        if (transform.localScale.y < limiter.MIN.y)
+        {
+            transform.localScale = new Vector3(transform.localScale.x, limiter.MIN.y, transform.localScale.z);
+        }
+        if (transform.localScale.y > limiter.MAX.y)
+        {
+            transform.localScale = new Vector3(transform.localScale.x, limiter.MAX.y, transform.localScale.z);
+        }
+
+        if (transform.localScale.x < limiter.MIN.x)
+        {
+            transform.localScale = new Vector3(limiter.MIN.x, transform.localScale.y, transform.localScale.z);
+        }
+        if (transform.localScale.x > limiter.MAX.x)
+        {
+            transform.localScale = new Vector3(limiter.MAX.x, transform.localScale.y, transform.localScale.z);
         }
     }
 }
